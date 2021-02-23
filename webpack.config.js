6b5bd6b52413a61add2ae1ced81988b76a33086e
kristen <studiosok@gmail.com>
2021-02-22 21:13:03 -0500

    CSS webpack added

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
+const path = require("path");
 
+module.exports = {
+  mode: "development",
+  entry: "./client/index.js",
+  devtool: "inline-source-map",
 
-  const path = require('path');
-
-
- module.exports = {
-   mode: 'development',
-   entry: './client/index.js', 
-   devtool: 'inline-source-map',
-
-   output: {
-     filename: 'bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
-   module: {
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
-    ]
-  }
- }
+    ],
+  },
+};
